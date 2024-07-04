c5921098771fdb11bb51dab33990ee7fee0205b6
rithvikgit <rithvik.r.poojary98@gmail.com>
2024-07-04 12:10:10 +0530

    First React Application Food App. Used Props, Map and fetched api's.

---
+++ Basics/App-jsx.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React Element
+
+//React.createElement => Object => HTMLElement(render)
+/**
+ * const heading = React.createElement("h1", { id: "heading" }, "Namste React");
+ * console.log(heading);
+ */
+
+
+//JSX :- it is not HTML in JS, It is HTML like syntax or XML like.
+//JSx :- (transpiled before it reached to JS)- Parcel - Babel.
+//JSX => React.CreateElement => ReactElement-JS Object => JTMLElement(render)
+const jsxHeading = <h1 id = "heading" className="head" tabIndex="5">Namste REact using JSX</h1>;
+
+console.log(jsxHeading);
+
+
+
+//React Component
+//Class Based Component - OLD
+//Functional Component - NEW
+
+//React Functional Component
+const Title = () => {
+    return <h1 id="heading">Namste React Functional Component title</h1>;
+};
+
+//Component Composition
+const HeadingComponent = () => (
+    <div id="container">
+        <Title/>
+        <Title></Title>
+        {Title()}
+        <h1 id="heading">Namste React Functional Component</h1>
+    </div> 
+);
+
+const HeadingComponent2 = () => <h1 id="heading">Namste React Functional Component</h1>;
+
+console.log(HeadingComponent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent/>);
